9ec8116d9a116ba8cb41de9585ab815bc7887f7b
saher785255 <saherhossamfakhry21@gmail.com>
2025-08-31 13:54:28 +0300

    tut--007 which involves indexing and chunking

--- src/models/enums/DataBaseEnum.py
+++ src/models/enums/DataBaseEnum.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
-class DataBaeEnum(Enum):
+class DataBaseEnum(Enum):
 
     COLLECTION_PROJECT_NAME = "projects"
     COLLECTION_CHUNK_NAME = "chunks"
-    
+    COLLECTION_ASSET_NAME = "assets"
